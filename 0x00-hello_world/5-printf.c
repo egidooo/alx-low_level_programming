a3f999ce7608ff863e3ad720d3ce665413f892e1
egidooo <kwizeraegide250@gmail.com>
2023-02-16 14:45:28 -0800

    hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing the string with printf function
+ * Return: program return to 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
