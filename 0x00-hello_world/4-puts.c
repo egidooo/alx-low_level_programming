c73f6a2d81ec3d3a7cc6eb3d61c61c4cc35db373
egidooo <kwizeraegide250@gmail.com>
2023-02-16 14:38:07 -0800

    hello puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - use of function puts to print the line
+ * Return: program return to 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
