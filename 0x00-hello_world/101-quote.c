747c78e8b82609d5b8ca3b6793f688452f004067
egidooo <kwizeraegide250@gmail.com>
2023-02-16 16:20:20 -0800

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - printing the line with putchar function
+ * Return: program returns to 0
+ */
+int main(void)
+{
+	putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (0);
+}
