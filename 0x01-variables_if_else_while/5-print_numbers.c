7f55bafe67dc876a9c208f6906507fe5c27d79ae
egidooo <kwizeraegide250@gmail.com>
2023-02-17 12:09:02 -0800

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - program that prints single number of base 10
+ * Return: program returns to 0
+ */
+int main(void)
+{
+	int p;
+
+	for (p = 0; p <= 10; p++)
+		printf("%d\n", p);
+return (0);
+}
