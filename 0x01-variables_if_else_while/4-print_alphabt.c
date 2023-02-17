3e6093cd3e1eefc0d05c1635e13be903fdcf179a
egidooo <kwizeraegide250@gmail.com>
2023-02-17 10:50:45 -0800

    lower case alphabet except e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - program that printslower case alphabet except e and q
+ * Return: program returns to 0
+ */
+int main(void)
+{
+	char p, e, q;
+
+	e = 'e';
+	q = 'q';
+
+	for (p = 'a'; p <= 'z'; p++)
+	{
+		if (p != 'e' && p != 'q')
+		putchar(p);
+		putchar('\n');
+	}
+return (0);
+}
