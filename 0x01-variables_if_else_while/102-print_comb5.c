23d5ae5b7ea7d86c641bbef3eefa6ab722d5b7c9
egidooo <kwizeraegide250@gmail.com>
2023-02-17 15:08:34 -0800

    combination 5

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - entry point of the program
+ * Return: program returns to 0
+ */
+
+int main(void)
+{
+	int k, e;
+
+	for (k = 0; k <= 98; k++)
+	{
+		for (e = k + 1; e <= 99; e++)
+		{
+			putchar((k / 10) + '0');
+			putchar((k / 10) + '0');
+			putchar(' ');
+			putchar((e / 10) + '0');
+			putchar((e % 10) + '0');
+			if (k == 98 && e == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
