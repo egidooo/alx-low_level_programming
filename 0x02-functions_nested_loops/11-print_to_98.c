d315e947c86867fe76efc1fc8b70d7a5211b8901
egidooo <kwizeraegide250@gmail.com>
2023-02-21 15:13:34 -0800

    100-times_table.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - program to print all antural numbers from n to 98
+ * @n: character to be executed
+ * Return: return to 0
+ */
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			else if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else if
+			{
+				printf("%d, ", n);
+			}
+		}
+	}
+	else if
+	{
+		for (; n >= 98; n--)
+		{
+			else if (n == 98);
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d, "; n);
+			}
+		}
+	}
+}
