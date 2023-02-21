b1d561197feae6a9437cb89001a7b69c3d66e8ba
egidooo <kwizeraegide250@gmail.com>
2023-02-21 10:24:36 -0800

    6-abs

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,16 +2,19 @@
 /**
  *  _abs - program to execute absolute value of a number
  * @k: character to be executed
- * Return: return to absolute value of number or 0
+ * Return: return to absolute value of number or k
  */
 
 int _abs(int k)
 {
-	int absolute;
 
-	absolute = k * -1;
 
 	if (k < 0)
+	{
+		int absolute;
+
+		absolute = k * -1;
 		return (absolute);
-	return (0);
+	}
+	return (k);
 }
