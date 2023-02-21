b271d56674dad3354f46ef22b0b01577778f6c34
egidooo <kwizeraegide250@gmail.com>
2023-02-21 15:27:22 -0800

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,29 +10,29 @@ void times_table(void)
 
 	for (a = 0; a <= 9; a++)
 	{
-		for (b = 0; b <= 9; b++)
-		{
-			c = a * b;
-			if (c > 9)
-			{
-				d = c % 10;
-				e = (a - d) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(e + '0');
-			 	 _putchar(d + '0');
-			}
-			else
-			{
-			if (b != 0)
-			{
-			_putchar(44);
-			_putchar(32);
-			_putchar(32);
-			}
-			_putchar(c + '0');
-			}
-		}
-		_putchar('\n');
+	for (b = 0; b <= 9; b++)
+	{
+	c = a * b;
+	if (c > 9)
+	{
+	d = c % 10;
+	e = (c - d) / 10;
+	_putchar(44);
+	_putchar(32);
+	_putchar(e + '0');
+	 _putchar(d + '0');
+	}
+	else
+	{
+	if (b != 0)
+	{
+	_putchar(44);
+	_putchar(32);
+	_putchar(32);
+	}
+	_putchar(c + '0');
+	}
+	}
+	_putchar('\n');
 	}
 }
