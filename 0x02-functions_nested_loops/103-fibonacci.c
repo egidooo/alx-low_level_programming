81462c4d37ab0cb9abd148faa36658321434aab5
egidooo <kwizeraegide250@gmail.com>
2023-02-21 18:28:55 -0800

    103-fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - program to print sum  first 11 fibonacci
+ * Return: returns to 0
+ */
+
+int main(void)
+{
+
+	int m = 0;
+	long n = 1, o = 2, sum = o;
+
+	while (0 + n < 4000000)
+	{
+	o += n;
+	if (o % 2 == 0)
+	sum += o;
+	n = o - n;
+	++m;
+	printf("%ld\n", sum);
+	}
+	return (0);
+}
