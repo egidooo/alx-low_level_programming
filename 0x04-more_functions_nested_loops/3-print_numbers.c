942801c1ca87e146abf532affe744972f0126244
egidooo <kwizeraegide250@gmail.com>
2023-02-23 05:58:36 -0800

    numbers from 0 to 9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_numbers - prints number from 0 tp 9
+ * Return: return to void
+ */
+
+void print_numbers(void)
+{
+	int p;
+
+	for (p = 0; p <= 9; p++)
+		_putchar(p);
+}
