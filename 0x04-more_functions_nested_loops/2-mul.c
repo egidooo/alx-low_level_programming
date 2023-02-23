83051bb41ab7da63b3caf4eb6fdec0f36284d658
egidooo <kwizeraegide250@gmail.com>
2023-02-23 05:45:20 -0800

    multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply two digits
+ * @a: first character
+ * @b: second character
+ * Return: return to mul
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
