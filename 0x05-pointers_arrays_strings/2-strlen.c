b860465b5219b8ce5f60e180a093138c988fcc30
egidooo <kwizeraegide250@gmail.com>
2023-02-27 06:40:12 -0800

    print a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strlen - function that returns the length of string
+ * @s: string character
+ * Returns: void
+ */
+
+int _strlen(char *s)
+{
+	strlen(*s);
+}
