2ce2ed6ef10e1914543dd6e7163ff1ff84e20d91
egidooo <kwizeraegide250@gmail.com>
2023-03-16 07:34:17 -0700

    C - More malloc, free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -5,7 +5,7 @@
  * *_realloc -  reallocates a memory block using malloc and free
  * @ptr: pointer
  * @old_size: size of allocated pointer
- * @new_size: new size 
+ * @new_size: new size
  * Return: pointer
  */
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
