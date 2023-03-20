37c877517addd10bf5835616b15776d9e6a70faf
egidooo <kwizeraegide250@gmail.com>
2023-03-20 07:52:23 -0700

    dog information

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - information of the dog
+ * @name: member 1
+ * @age: member 2
+ * @owner: member 3
+ * Description: longer discription
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
